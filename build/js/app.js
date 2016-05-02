74c7fa6fff274aea115b20413af87f5f7ccfb52b
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-05-01 19:58:58 -0700

    Added api call and updated messages according to data results

--- build/js/app.js
+++ build/js/app.js
@@ -47,11 +47,45 @@
 	var angular = __webpack_require__(1);
 
 	angular.module('flickrApp', [])
-	  .controller('flickrCtrl', ['$scope', function($scope){
+	  .config(function($httpProvider) {
+	    $httpProvider.defaults.useXDomain = true;
+	    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	  })
+	  .controller('flickrCtrl', ['$scope', '$http', function($scope, $http){
 	    $scope.valid = false;
+	    $scope.error = false;
 
-	    $scope.searchFlickr = function(){
-	      if($scope.tag){
+	    //Gets photos from Flickr API and updates the view
+	    //with results or any error messages.
+	    function getPhotos(){
+	      var url = 'https://api.flickr.com/services/rest';
+	      var params = {
+	        method: 'flickr.photos.search',
+	        api_key: '530a7056fd1d44589661f5c0dfb8fb21',
+	        tags: $scope.word,
+	        format: 'json',
+	        nojsoncallback: 1
+	      };
+
+	      $http({
+	        method: 'GET',
+	        url: url,
+	        params: params
+	      })
+	      .then(function(response) {
+	        $scope.results = response.data.photos;
+	        $scope.error = false;
+	        console.log(response.data.photos);
+	      },
+	      function(response) {
+	        $scope.error = true;
+	      });
+	    }
+
+	    //Sets the tag provided by the user and calls 
+	    //function to start api search.
+	    $scope.searchFlickr = function(word){
+	      if(word){
 	        $scope.valid = true;
 	      } else { 
 	        $scope.search.$setPristine();
@@ -59,8 +93,10 @@
 	        return;
 	      }
 
-	      $scope.word = $scope.tag;
+	      $scope.word = word;
 	      $scope.tag = '';
+	      $scope.results = '';
+	      getPhotos();
 	    };
 	  }]);
 
